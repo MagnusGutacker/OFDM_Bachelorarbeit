f0040191e328bee870f602a4ab48328f822cf0a3
Magnus <Magnus@MagnusPC>
2021-11-08 15:36:47 +0100

    now using reshape function

--- serial_to_parallel.m
+++ serial_to_parallel.m
@@ -4,14 +4,14 @@ function parallel_data = serial_to_parallel(serial_data , N_sub  , symbol_size)
 %   Detailed explanation goes here
 
 parallel_data = zeros(N_sub,int32(length(serial_data)/(N_sub*symbol_size)),symbol_size);
-counter = 1;
-    for i = 1:length(serial_data) / (N_sub * symbol_size)       
-        for j = 1 : N_sub
-            parallel_data(j,i,:) = serial_data(counter:counter+symbol_size-1);
-            counter = counter + symbol_size;
-        end
-    end
-
+% counter = 1;
+%     for i = 1:length(serial_data) / (N_sub * symbol_size)       
+%         for j = 1 : N_sub
+%             parallel_data(j,i,:) = serial_data(counter:counter+symbol_size-1);
+%             counter = counter + symbol_size;
+%         end
+%     end
+parallel_data = reshape(serial_data,symbol_size,N_sub,[]);
+parallel_data = permute(parallel_data,[2 3 1]);
 end
 
-%parallel_data = reshape(serial_data,N_sub,length(serial_data)/(N_sub*symbol_size),symbol_size);
