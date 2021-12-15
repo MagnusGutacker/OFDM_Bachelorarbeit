78598b4f5eba56e90ad91a16883d3936bc824415
MagnusGutacker <93678474+MagnusGutacker@users.noreply.github.com>
2021-12-15 10:24:15 +0100

    Add files via upload

--- MMSE.m
+++ MMSE.m
@@ -1,41 +1,41 @@
-function output = MMSE(data,taps,SNR)
+function output = MMSE(data,taps,delays,SNR)
 %MMSE Summary of this function goes here
 %   Detailed explanation goes here
 
-% L = length(taps)-1;
-% M = length(taps)+1;
-% H = zeros(L+M+1,M+1);
-% 
-% for i = 1:M+1
-%     H(i:i+length(taps)-1,i) = taps; 
-% end
-% H(L+M:L+M+1,:) = [];
-% H(1:2,:) = [];
-% H_H = conj(H');
-% 
-% I = zeros(M+1,1);
-% I(ceil((M+1)/2))=1;
-% 
-% SNR = db2mag(SNR);
-% f = inv(H_H*H + (1/SNR)*eye(M+1))*H_H*I;
-% 
-% output = filter(f,1,data);
+TD_array = zeros(delays(end)+1,1);
+for i = 1:length(delays)
+    TD_array(delays(i)+1) = taps(i);
+end
+% H = convmtx(TD_array,length(TD_array));
+% H(length(H(1,:))+1:end,:) = [];
+for i = 1:length(TD_array)+2
+    H(i:i+length(TD_array)-1,i) = TD_array;
+%     if(i+length(TD_array)-1>length(TD_array)+2)
+%         
+%     end
+end
 
-% M = length(taps)+30;
+H(length(H(1,:))+1:end,:) = [];
+H_H = conj(H');
+SNR = db2mag(SNR);
+F = (H_H*H + (1/(SNR))*eye(length(H(1,:))))^(-1)*H_H;
+f = F(1,:);
+
+% M = length(taps)+2;
 % 
 % H = convmtx(taps,M)';
-% H(M-1:M,:) = [];
-% H(1:2,:) = [];
+% H(7,:) = [];
+% H(1,:) = [];
 % I = zeros(size(pinv(H),2), 1,1);
-% I(1)=1;
+% I(1)=2;
 % 
-% H_H = conj(H');
+% H_H = conj(transpose(H));
 % 
 % SNR = db2mag(SNR);
-% f = pinv(H_H*H + (1/SNR)*eye(M))*H_H*I;
-
-SNR = db2mag(SNR);
-f = taps./(abs(taps).^2+SNR);
+% f = (H*H_H + (1/SNR)*eye(M))^(-1)*H_H;
+% 
+% SNR = db2mag(SNR);
+% f = taps./(abs(taps).^2+SNR);
 
 output = conv(f,data);
 output(length(data)+1:end) = [];
