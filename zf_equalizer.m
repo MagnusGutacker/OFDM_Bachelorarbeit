104ac567f8d04549231d8c8fdf795634a197f8ca
Magnus <Magnus@MagnusPC>
2021-12-15 10:19:15 +0100

    working

--- zf_equalizer.m
+++ zf_equalizer.m
@@ -1,8 +1,8 @@
-function output = zf_equalizer(data,taps)
+function output = zf_equalizer(data,taps,delays)
 %ZF_EQUALIZER Summary of this function goes here
 %   Detailed explanation goes here
 
-% M = length(taps)+4;
+% M = length(taps)+2;
 % 
 % H = convmtx(taps,M)';
 % H(M,:) = [];
@@ -11,7 +11,14 @@ function output = zf_equalizer(data,taps)
 % I = zeros(size(pinv(H),2), 1,1);
 % I(3)=1;
 
-f = pinv(taps);
+TD_array = zeros(delays(end)+1,1);
+for i = 1:length(delays)
+    TD_array(delays(i)+1) = taps(i);
+end
+H = convmtx(TD_array,length(TD_array));
+H(length(H(1,:))+1:end,:) = [];
+H_inv = H^-1;
+f = H_inv(:,1)';
 
 output = conv(f,data);
 output(length(data)+1:end) = [];
