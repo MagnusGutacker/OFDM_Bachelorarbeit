a91bcf061fb5a9edf0da8de4e0b6e6cb1246d777
magnusgutacker <magnus.gutacker@gmail.com>
2022-01-06 17:29:16 +0100

    .

--- MMSE.m
+++ MMSE.m
@@ -1,41 +1,20 @@
-function output = MMSE(data,taps,SNR)
+function output = MMSE(data,taps,delays,SNR)
 %MMSE Summary of this function goes here
 %   Detailed explanation goes here
 
-% L = length(taps)-1;
-% M = length(taps)+1;
-% H = zeros(L+M+1,M+1);
-% 
-% for i = 1:M+1
-%     H(i:i+length(taps)-1,i) = taps; 
-% end
-% H(L+M:L+M+1,:) = [];
-% H(1:2,:) = [];
-% H_H = conj(H');
-% 
-% I = zeros(M+1,1);
-% I(ceil((M+1)/2))=1;
-% 
-% SNR = db2mag(SNR);
-% f = inv(H_H*H + (1/SNR)*eye(M+1))*H_H*I;
-% 
-% output = filter(f,1,data);
+TD_array = zeros(delays(end)+1,1);
+for i = 1:length(delays)
+    TD_array(delays(i)+1) = taps(i);
+end
 
-% M = length(taps)+30;
-% 
-% H = convmtx(taps,M)';
-% H(M-1:M,:) = [];
-% H(1:2,:) = [];
-% I = zeros(size(pinv(H),2), 1,1);
-% I(1)=1;
-% 
-% H_H = conj(H');
-% 
-% SNR = db2mag(SNR);
-% f = pinv(H_H*H + (1/SNR)*eye(M))*H_H*I;
-
+for i = 1:length(TD_array)+2
+    H(i:i+length(TD_array)-1,i) = TD_array;
+end
+%H(length(H(1,:))+1:end,:) = [];
+H_H = conj(H');
 SNR = db2mag(SNR);
-f = taps./(abs(taps).^2+SNR);
+F = (H_H * H + (1/(SNR))*eye(length(H(1,:))))^(-1)*H_H;
+f = F(:,1);
 
 output = conv(f,data);
 output(length(data)+1:end) = [];
--- OFDM.m
+++ OFDM.m
@@ -1,42 +1,49 @@
 clc;
 clear;
-%OFDM
 %% Declare parameters
 
-signal_in_passband = false;      %true for frequency modulation
+signal_in_passband = false;     %true for frequency modulation
 
 N_sub = 64;                     %Anzahl der Unterträger
 symbol_size = 2;                %Symbolgröße z.B. 2 für 4-Qam oder 4 für 16-QAM
-signal_length = 1000;            %Signallänge in OFDM Symbolen
+signal_length = 1000;           %Anzahl der OFDM Symbole
 cp_size = ceil(N_sub/8);        %cyclic prefix Länge                
 fc = 5e9;                       %Trägerfrequenz
 fs = 4e7;                       %Bandbreite
-SNR = -10:3:20;                       %Varianz/SNR
-taps = [1,0.4,0.3];%Gewichtung der einzelnen Verzögerungen
-delays = [0,1,2];           %Verzögerungen des Kanals
+SNR = -10:2:20;                 %Varianz/SNR
+taps = [1,0.4,0.3];             %Gewichtung der einzelnen Verzögerungen
+delays = [0,1,2];               %Verzögerungen des Kanals
+ph_off = pi/16;                  %Phasen offset
+freq_off = 0.1;                 %Frequenz offset (bezogen auf T)
+a_off = 1.5;                    %Amplituden offset
+IQ_ph_off = pi/8;               %Phasen offset bei IQ_imbalance
 
+pilot_length = 1;
+pilot = serial_to_parallel(randi([0 1],1,N_sub * symbol_size * pilot_length),N_sub,symbol_size);
+pilot = QAM(pilot);
 
 %% generate signal
 
 input_signal = randi([0 1],1,N_sub * signal_length * symbol_size);
 
 %% serial to parallel
-%konvertiert den seriellen Datenstrom in N_sub parallele Datenströme
 
 parallel = serial_to_parallel(input_signal,N_sub,symbol_size);
 
 %% QAM
-%Bits werden in QAM-Symbole moduliert
 
 [QAM_modulated,norm]  = QAM(parallel);
 
-%% IFFT
-%Die IFFT der parallelen Symbole wird berechnet
+%% add pilots
 
-for j = 1 : length(QAM_modulated(1,:))
-    x = j * length(QAM_modulated(:,1)) - length(QAM_modulated(:,1)) + 1;
-    y = j * length(QAM_modulated(:,1));
-    ifft_array (x:y) = ifft(QAM_modulated(:,j));
+signal_pilot = [pilot,QAM_modulated];
+
+%% IFFT
+
+for j = 1 : length(signal_pilot(1,:))
+    x = j * length(signal_pilot(:,1)) - length(signal_pilot(:,1)) + 1;
+    y = j * length(signal_pilot(:,1));
+    ifft_array (x:y) = ifft(signal_pilot(:,j));
 end
 
 %% cyclic prefix
@@ -48,55 +55,70 @@ cp = cyclic_prefix(ifft_array,cp_size,N_sub);
 if (signal_in_passband)
     cp = upconversion(cp,fs,fc);
 end
+
+pilot_modulated = cp(1:N_sub * pilot_length);
+
 %% Channel
 for i = 1:length(SNR)   %Calculate for each SNR
     %AWGN-Channel
     channel_array = AWGN_channel(cp,SNR(i));
 
     for a = 1:4         %Calculate for AWGN(a=1) TD(a=2) TD-zf(a=3) TD-MMSE(a=4)
-        equalized = channel_array;
-        if(a==2||a==3||a==4)    %apply TD-Channel
-            channel_array_out = tapped_delay_channel(channel_array,taps,delays);
-
-            if (a==3)           %ZF-Equalization
-                equalized = zf_equalizer(channel_array_out,taps);
-            elseif (a==4)       %MMSE-Equalization
-                equalized = MMSE(channel_array_out,taps,SNR(i));
-            else                %No Equalization
-                equalized = channel_array_out;
+        channel_array_out = channel_array;
+        if(a==2||a==3||a==4) 
+            if (signal_in_passband)
+                delays_channel = delays * 50;   %Bei Signal im Passband wird das Signal um den Faktor 50 upgesampled, deshalb müssen die Delays auch 50 mal größer sein
+            else
+                delays_channel = delays;
             end
-
+            %Tapped-delay-channel
+            channel_array_out = tapped_delay_channel(channel_array,taps,delays_channel);
         end
-
         %% shift to baseband
-        baseband_signal = reshape(equalized,1,[]);
+
+        baseband_signal = reshape(channel_array_out,[],1);
         if (signal_in_passband)
             baseband_signal = downconversion(baseband_signal,fs,fc);
         end
+
+        %% Equalization 
+
+        if (a==3)           %ZF-Equalization
+            equalized = zf_equalizer(baseband_signal,taps,delays);
+        elseif (a==4)       %MMSE-Equalization
+            equalized = MMSE(baseband_signal,taps,delays,SNR(i));
+        else                %No Equalization
+            equalized = baseband_signal;
+        end
+
         %% remove cyclic prefix
 
-        no_cp = remove_cp (baseband_signal,cp_size,N_sub);
+        no_cp = remove_cp (equalized,cp_size,N_sub);
 
         %% FFT
-        %FFT wird von einem OFDM Symbol gebildet, das die Länge von N_sub hat
 
         ifft_array_parallel = serial_to_parallel(no_cp,N_sub,1);
-
-        for j = 1 : (signal_length)
+        for j = 1 : (signal_length+pilot_length)
             x = j * N_sub - N_sub + 1;
             y = j * N_sub;
             fft_array(x:y) = fft(ifft_array_parallel(:,j));
         end
 
-        %% demodulate QAM
-        %QAM-Symbole werden in Bits demoduliert
+        %% Channel Estimation
+        fft_array_off = phase_offset(fft_array,ph_off);
 
-        QAM_demodulated = QAM_demod(fft_array,symbol_size,norm);
+        H = LS_estimator(pilot,fft_array_off);
+        %% remove pilot
+
+        equalized = fft_array_off (pilot_length*N_sub+1:end);
+
+        %% demodulate QAM
+
+        QAM_demodulated = QAM_demod(equalized,symbol_size,norm);
 
         %% parallel to serial
 
         output_signal = parallel_to_serial(QAM_demodulated);
-
 
         %% Fehlerraten
         %BER
@@ -109,33 +131,36 @@ for i = 1:length(SNR)   %Calculate for each SNR
         numberOfZeros = sum(SymbolFehler(:)==0);
         SER = 1 - numberOfZeros/length(SymbolFehler);
 
-        %% test plots
-        % if (signal_in_passband)
-        %     figure('Name' , "Plots");
-        %     hold on;
-        %     title('Passband Signal');
-        %     xlabel('Frequenz f in GHz');
-        %     ylabel('Amplitude');
-        %     z = interp(cp,2);
-        %     f = -(fs*500)/(2):fs*500/(length(z)):(fs*500)/(2)-1;
-        %     f = f/1000000000; %GHz
-        %     plot(f,fftshift(abs(fft(z))));
-        %     hold off;
-        % end
     end
-end
+end        
+
+%% test plots
+
+% if (signal_in_passband)
+%     figure('Name' , "Plots");
+%     hold on;
+%     title('Passband Signal');
+%     xlabel('Frequenz f in GHz');
+%     ylabel('Amplitude');
+%     z = interp(cp,2);
+%     f = -(fs*500)/(2):fs*500/(length(z)):(fs*500)/(2)-1;
+%     f = f/1000000000; %GHz
+%     plot(f,fftshift(abs(fft(z))));
+%     hold off;
+% end
 figure('Name',"BER of equalized and not equalized Signal");
-subplot(4,1,[1 3]);
+%subplot(5,1,[1 3]);
 hold on;
 xlabel("SNR in dB");
 ylabel("BER");
 set(gca,'YScale','log')
-axis([ SNR(1) SNR(end) 10^(-5) 1]);
+axis([ SNR(1) SNR(end) 1/(signal_length*N_sub) 1]);
 plot(SNR,BER);
 legend({'AWGN-Channel','TD without zf','TD with zf','TD with MMSE'},'Location','southwest')
-subplot(4,1,4);
-xlabel("Delays");
-ylabel("Taps");
-stem(taps);
+% subplot(5,1,5);
+% axis([ -1 delays(end)+1 0 1.2]);
+% xlabel("Delays");
+% ylabel("Taps");
+% stem(taps);
 %scatterplot(reshape(QAM_modulated,1,[]));
 %scatterplot(fft_array);
