943d132b025d84d29d65817065d4f9587024b00e
magnusgutacker <magnus.gutacker@gmail.com>
2022-02-01 14:34:59 +0100

    fixed big angles

--- phase_offset_eq.m
+++ phase_offset_eq.m
@@ -2,7 +2,13 @@ function output = phase_offset_eq(data,pilot)
 %PHASE_PFFSET_EQ Summary of this function goes here
 %   Detailed explanation goes here
 data1 = data(1:length(pilot));
-ph_off_est = mean(angle(data1)-angle(pilot));
+angles = angle(data1)-angle(pilot);
+for i = 1:length(pilot)
+if(angles(i)<-2)
+    angles(i) = angles(i) + 2*pi;
+end
+end
+ph_off_est = mean(angles);
 output = exp(-1i * ph_off_est)*data;
 
 
